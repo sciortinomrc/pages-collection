0bbfdc1bfff428237976ffa4e4c7dbe8d6bef174
Marco <sciortino.mrc@gmail.com>
2018-10-30 17:25:23 +0000

    should work now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import {createStore,applyMiddleware ,combineReducers} from 'redux';
-import {createLogger} from 'redux-logger';
+// import {createLogger} from 'redux-logger';
 import {
 	onLogin, fbApiCall, 
 	fbLogin, onWindowResize, 
@@ -15,13 +15,12 @@ import './index.css';
 import WindowFB from './Containers/WindowFB';
 import registerServiceWorker from './registerServiceWorker';
 
-const logger=createLogger();
+// const logger=createLogger();
 const rootReducer=combineReducers({
 	onLogin, fbApiCall, fbLogin, 
 	onWindowResize, onPageChange, addNewPage,
 	displaySingleCard, addFilter, signUp})
-const store=createStore(rootReducer,applyMiddleware(thunkMiddleware, logger));
-
+const store=createStore(rootReducer,applyMiddleware(thunkMiddleware /*logger*/
 
 ReactDOM.render(
 	<Provider store={store}>
