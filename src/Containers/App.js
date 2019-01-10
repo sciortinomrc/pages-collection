11a16ccf499c5043f36eef74bbbe52f2bdc13c28
Marco <sciortino.mrc@gmail.com>
2019-01-10 18:29:47 +0000

    edit

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -133,7 +133,7 @@ componentDidMount(){
           case 'display': return ( <DisplayPages category={category} database={database}/> )
           case 'card': return (<div className="d-flex m-auto justify-content-center">{this.displayReceivedCard(this.state.cardToDisplay)}</div>)
           case 'favourites': return( <DisplayPages category='favourites' database={this.filterFavourites()}/>)
-          case 'user': return(<UserPanel database={database} user={user} name={this.state.userName.name}/>)
+          case 'user': return(<UserPanel database={database} user={user} name={this.state.userName}/>)
           default: return( <h1> ... The page is Loading ...</h1> )
       }
   }
